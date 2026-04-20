33efe7b2249ab811a0632d103182d62639ec215c
dat.tran <dattran0808@gmail.com>
2026-04-20 16:31:47 +0700

    Framework 08 - Build BasePage class - Part 4

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,8 +1,13 @@
 package commons;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 
 public class BasePage {
     //1 - Access Modifier: public/ protected/ private/ default
@@ -15,22 +20,131 @@ public class BasePage {
     //5 - Kiểu dữ liệu trả về cho hàm (liên quan đến các step mình viết trong hàm đó)
     //    Nếu như có return dữ liệu thì sẽ khớp vs kiểu dữ liệu ở số 2
     //    Nếu như có return thì nó là cái step cuối cùng
-    private WebDriver driver;
 
-    public boolean isElemenetDisplayed() {
-        return driver.findElement(By.cssSelector("")).isDisplayed();
+    //Common function (hàm dùng chung) cho nhiều class khác
+    public void openPageUrl(WebDriver driver, String url) {
+        driver.get(url);
     }
 
-    public void clickToElement() {
-        driver.findElement(By.cssSelector("")).click();
+    public String getPageTitle(WebDriver driver) {
+        return driver.getTitle();
     }
 
-    public void getElementText() {
-        driver.findElement(By.cssSelector("")).getText();
+    public String getPageUrl(WebDriver driver) {
+        return driver.getCurrentUrl();
     }
 
-    //Giá trị sử dụng trong hàm sẽ truyền từ bên ngoài thông qua các tham số
-    public void sendKeyToElement(String value) {
-        driver.findElement(By.cssSelector("")).sendKeys(value);
+    public void backToPage(WebDriver driver) {
+        driver.navigate().back();
     }
+
+    public void forwardToPage(WebDriver driver) {
+        driver.navigate().forward();
+    }
+
+    public void refreshCurrentPage(WebDriver driver) {
+        driver.navigate().refresh();
+    }
+
+    public Alert waitForAlertPresence(WebDriver driver) {
+        return new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.alertIsPresent());
+    }
+
+    public void acceptToAlert(WebDriver driver) {
+        waitForAlertPresence(driver).accept();
+    }
+
+    public void cancelToAlert(WebDriver driver) {
+        driver.switchTo().alert().dismiss();
+    }
+
+    public String getAlertText(WebDriver driver) {
+        return waitForAlertPresence(driver).getText();
+    }
+
+    public void sendkeyToAlert(WebDriver driver, String keysToSend) {
+        waitForAlertPresence(driver).sendKeys(keysToSend);
+    }
+
+    public void switchToWindowByID(WebDriver driver, String parentID) {
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String runWindow : allWindows) {
+            if (!runWindow.equals(parentID)) {
+                driver.switchTo().window(runWindow);
+                break;
+            }
+        }
+    }
+
+    public void switchToWindowByTitle(WebDriver driver, String title) {
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String runWindow : allWindows) {
+            driver.switchTo().window(runWindow);
+            String currentWin = driver.getTitle();
+            if (currentWin.equals(title)) {
+                break;
+            }
+        }
+    }
+
+    public void closeAllWindowsWithoutParent(WebDriver driver, String parentID) {
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String runWindow : allWindows) {
+            if (!runWindow.equals(parentID)) {
+                driver.switchTo().window(runWindow);
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentID);
+    }
+
+    public WebElement getElement(WebDriver driver, String locator) {
+        return driver.findElement(By.xpath(locator));
+    }
+
+    public void clickToElement(WebDriver driver, String locator) {
+        getElement(driver, locator).click();
+    }
+
+    public void sendkeyToElement(WebDriver driver, String locator, String value) {
+        getElement(driver, locator).sendKeys(value);
+    }
+
+    public void selectItemInDropdown(WebDriver driver, String locator, String itemText) {
+        new Select(getElement(driver, locator)).selectByVisibleText(itemText);
+    }
+
+    public String getSelectedItemInDropdown(WebDriver driver, String locator) {
+        return new Select(getElement(driver, locator)).getFirstSelectedOption().getText();
+    }
+
+    public boolean isDropdownMultiple(WebDriver driver, String locator) {
+        return new Select(getElement(driver, locator)).isMultiple();
+    }
+
+    public void selectItemInCustomDropdown(WebDriver driver, String parentLocator, String childItemLocator, String expectedItem) {
+        getElement(driver, parentLocator).click();
+        sleepInSecond(2);
+
+        List<WebElement> allItems = new WebDriverWait(driver, Duration.ofSeconds(15))
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy((By.xpath(parentLocator))));
+
+        sleepInSecond(2);
+        for (WebElement item : allItems) {
+            if (item.getText().trim().equals(expectedItem)) {
+                item.click();
+                break;
+            }
+        }
+    }
+
+    public void sleepInSecond(long timeInSeconds) {
+        try {
+            Thread.sleep(timeInSeconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
