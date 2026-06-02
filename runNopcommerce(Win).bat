e643cc1ad8ec2b01d210945cf800e1cde6f983f7
dat.tran <dattran0808@gmail.com>
2026-06-02 17:56:11 +0700

    update runCommerce.bat

--- runNopcommerce(Win).bat
+++ runNopcommerce(Win).bat
@@ -1,8 +1,9 @@
 @echo off
 title Starting nopCommerce
+cd /d "D:\Automation Testing\01-Software\nopCommerce_4.90.4_NoSource_win_x64"
 echo Running server nopCommerce... Please wait!
 echo.
 
-dotnet Nop.Web.dll --urls "http://localhost:5050"
+dotnet Nop.Web.dll --urls "http://localhost:5000"
 
 pause
